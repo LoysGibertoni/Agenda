6297264413695f9113d4ec03685270e937f921e0
Loys <loys.gibertoni@zarpsystem.com.br>
2018-12-02 19:34:50 -0200

    Correção da dialog de seleção de data

--- app/src/main/java/br/edu/ifspsaocarlos/agenda/activity/DetalheActivity.java
+++ app/src/main/java/br/edu/ifspsaocarlos/agenda/activity/DetalheActivity.java
@@ -3,7 +3,6 @@ package br.edu.ifspsaocarlos.agenda.activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
